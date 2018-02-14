5fa3e16f9e0c8ee3a027798fcd5bdc3e4b89a39c
Olga <olgapoddubko@gmail.com>
2018-02-14 13:39:53 +0300

    implemented authentication

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ mongoose.connect('mongodb://editor:editorspass@ds125318.mlab.com:25318/blogsdb')
 .then(() =>  console.log('connection succesful'))
 .catch((err) => console.error(err));
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var blogs = require('./routes/blogs');
@@ -20,6 +23,20 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+var User = require('./models/User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -31,22 +48,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', users);
 app.use('/blogs', blogs);
-app.use('*', index);
+app.use('/', index);
 
 // catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+*/
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
